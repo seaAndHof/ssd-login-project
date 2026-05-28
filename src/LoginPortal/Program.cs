686cc0eb9b669e07e251519ced4af3238ad5a613
NillasKA <kramernicklas@gmail.com>
2026-05-28 18:10:04 +0200

    Updating program.cs to include identity and EF Core.

--- src/LoginPortal/Program.cs
+++ src/LoginPortal/Program.cs
@@ -1,4 +1,15 @@
+using LoginPortal.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddRazorPages();
 builder.Services.AddDistributedMemoryCache();
@@ -8,6 +19,8 @@ var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseSession();
 app.MapRazorPages();
 
